be2e4114a1aed55248a6cca19fb626cf77d957b1
flavie-yuan-liu <69368149+flavie-yuan-liu@users.noreply.github.com>
2021-09-16 17:30:37 +0200

    main_func

--- demo_dL_attack.py
+++ demo_dL_attack.py
@@ -1,79 +1,149 @@
+import argparse
 import os
 import torch
-from torchvision.transforms import transforms
-from MyImageNet import MyImageNet
+import torchvision.models as models
 from torch.utils.data import random_split
-from attacks.DL_attack import dict_training
-from evaluation import evl
+from attacks.dictionary_attack import ADIL
+import numpy as np
+import simulations.performance as perf
+from DS_ImageNet import DS_ImageNet
+import random
+from torch.utils.data import Subset
+import torchmetrics
+
+
+def dataset_split_by_class(dataset, number_per_class):
+
+    # number_of_class is list containing two elements,
+    # i.e., number_of_class = [number_per_class_train, number_per_class_val, number_per_class_test]
+
+    samples = dataset.samples
+    labels = [l for (_, l) in samples]
+    sorted_idx = np.argsort(labels)
+    num_classes = len(dataset.classes)
+
+    # this only works for the imagenet validation set with 50 samples per class
+    matrix_sorted_idx = sorted_idx.reshape([num_classes, 50])
+
+    split1 = number_per_class[0]+number_per_class[1]
+    split2 = sum(number_per_class)
+
+    for i in range(matrix_sorted_idx.shape[0]):
+        random.shuffle(matrix_sorted_idx[i, :])
+
+    indices_train = matrix_sorted_idx[:, 0:number_per_class[0]].flatten()
+    indices_val = matrix_sorted_idx[:, number_per_class[0]:split1].flatten()
+    indices_test = matrix_sorted_idx[:, split1:split2].flatten()
+
+    return Subset(dataset, indices_train), Subset(dataset, indices_val), Subset(dataset, indices_test)
 
 
 def load_ImageNet():
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
 
-    # Attention: this transformation should be consistent with that used for model training
-    trans = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomCrop(224),
-        transforms.ToTensor(),
-        normalize
-    ])
-
-    # Set root file path for ILSVRC2012 validation set
-    imagenet_root = './data/ImageNet/ILSVRC/Data'
+    # Set data path for ILSVRC2012 validation set
+    imagenet_file = './data/ImageNet/ImageNet1000.bin'
 
     # load ImageNet dataset, the validation set is used here for dictionary training
-    dataset = MyImageNet.MyImageNet(root=imagenet_root, split='val', transform=trans)
+    dataset = torch.load(imagenet_file)
     num_data = len(dataset)
+    clsses = dataset.classes
+
+    return dataset, clsses
+
+
+def model_accuracy(dataset, model, device='cpu'):
+    metric = torchmetrics.Accuracy()
+    metric.to(device)
+    model.eval()
+    with torch.no_grad():
+        test_loader = torch.utils.data.DataLoader(dataset, batch_size=256)
+        for x, y in test_loader:
+            x, y = x.to(device), y.to(device)
+            model = model.to(device)
+            pred = model(x)
+            acc = metric(pred, y)
+        acc = metric.compute()
+    metric.reset()
+    return acc
+
+
+def main(args):
+
+    if not torch.cuda.is_available():
+        print('Check cuda setting for model training on ImageNet')
+        return
+    device = 'cuda'
+
+    # ------------------------------------------------------------------------
+    # loading model (densenet, googlenet, inception, mobilenetv2, resnet, vgg)
+    # ------------------------------------------------------------------------
+
+    model_name = args.model.lower()
+    if model_name == 'resnet':
+        model = models.resnet18(pretrained=True, progress=False)
+    elif model_name == 'densenet':
+        model = models.densenet121(pretrained=True, progress=False)
+    elif model_name == 'googlenet':
+        model = models.googlenet(pretrained=True, progress=False)
+    elif model_name == 'inception':
+        model = models.inception_v3(pretrained=True, progress=False)
+    elif model_name == 'mobilenet':
+        model = models.mobilenet_v2(pretrained=True, progress=False)
+    elif model_name == 'vgg':
+        model = models.vgg11(pretrained=True, progress=False)
+
+    # ----------------------------------------------------------------------
+    # loading imagenet data
+    # ----------------------------------------------------------------------
+
+    dataset, classes = load_ImageNet()
+    acc = model_accuracy(dataset, model, device=device)
+    print("accuracy of the the model {} is {}".format(model_name, acc*100))
 
     # Set the number of samples for training
-    train_size = 128# int(0.1 * num_data)
-    train_dataset, test_dataset = random_split(dataset,
-                                             [train_size, num_data - train_size])
+    num_train_per_class = 10  # set the number of samples for training 10 x number of classes
+    num_val_per_class = 2
+    num_test_per_class = 5
 
-    test_loader = torch.utils.data.DataLoader(
-        dataset=test_dataset, shuffle=True, batch_size=128  # , num_workers=0, pin_memory=True
-    )
+    # train_dataset, test1_dataset = random_split(dataset, [num_train_set, num_data-num_train_set])
+    # val_dataset, test_dataset = random_split(test1_dataset, [num_val_set, num_data-num_train_set-num_val_set])
 
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset, batch_size=128, shuffle=False  # , num_workers=0, pin_memory=True
-    )
+    train_dataset, val_dataset, test_dataset \
+        = dataset_split_by_class(dataset, [num_train_per_class, num_val_per_class, num_test_per_class])
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, shuffle=False)
 
-    return train_loader, test_loader
+    # ----------------------------------------------------------------------
+    # hyper-parameter selecting
+    # ----------------------------------------------------------------------
+    lambda_grid = np.logspace(start=-5, stop=-1, num=4)
+    n_atoms_grid = np.array([20, 50])
+
+    attacks_hyper = {
+        'ADiL': perf.get_atks(model.to(device), ADIL, 'lambda_l1', lambda_grid, 'lambda_l2', lambda_grid,
+                              'n_atoms', n_atoms_grid, version='stochastic', data_train=train_dataset,
+                              device=device, batch_size=128, model_name=model_name, steps=5e2)
+    }
 
 
 if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '--model', '-m',
+        metavar='M',
+        default='resnet',
+    )
+    argparser.add_argument(
+        '--seed', '-s',
+        metavar='S',
+        type=int,
+        default=1,
+        help='change seed to carry out the exp'
+    )
+    args = argparser.parse_args()
 
-    if torch.cuda.is_available():
+    seed = args.seed  # Do from 1 to 5
+    torch.random.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
 
-        dataset_name = 'ImageNet'
-        model_name = 'ResNet'
-
-        # load data
-        train_loader, test_loader = load_ImageNet()
-
-        # load model (By default, we load the trained ResNet18 on ImageNet)
-        model = torch.load('./models/ImageNet_ResNet.model')
-
-        l2_fool, l1_sparse = 1e-4, 1e-4
-        folder_name = f"./models"
-        filename_D = f"Dict_{dataset_name}_{model_name}_LamReg_{l2_fool}_LamCoding_{l1_sparse}.bin"
-        fullpath = os.path.join(folder_name, filename_D)
-        # train dictionary for attack if DL attack model not exist
-        if not os.path.exists(fullpath):
-            dict_training(train_loader, model, fullpath, lambdaCoding=l1_sparse, lambdaReg=l2_fool, stepsize=.1, niter=20)
-
-        # load D
-        D, _ = torch.load(fullpath)
-
-        # test DL attack model's fooling rate
-        test_images, label = next(iter(test_loader))
-        point, param = evl(test_images, label, D, model, l2_fool, l1_sparse)
-
-
-    else:
-        print("Error: this project need to be run on GPU")
-
-
-
-
+    main(args)
